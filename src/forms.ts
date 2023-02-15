09e921e214c35754180c30a148dfcb1c210fd680
bf215181b5140 <108577982+bf215181b5140@users.noreply.github.com>
2023-02-15 16:31:20 +0100

    Update formField

--- src/forms.ts
+++ src/forms.ts
@@ -6,17 +6,17 @@ export class FormMeta {
 }
 
 export class FormField {
-    name: string | undefined | null;
-    label: string | undefined | null;
+    name: string;
+    label: string;
     description: string | undefined | null;
-    type: string | undefined | null;
+    type: string;
     required: boolean | undefined | null;
     minLength: number | undefined | null;
     maxLength: number | undefined | null;
     pattern: string | undefined | null;
-    order: number | undefined | null;
-    editable: boolean | undefined | null;
-    visible: boolean | undefined | null;
+    order: number;
+    editable: boolean;
+    visible: boolean;
     options: FieldOption[] | undefined | null;
 }
 
