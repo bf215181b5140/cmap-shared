698b5ce84cec6099a7e7497b71759b76de53cd99
bf215181b5140 <108577982+bf215181b5140@users.noreply.github.com>
2023-03-23 18:08:16 +0100

    Redid shared

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,100 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.InputType = exports.FieldOption = exports.FormField = exports.FormMeta = exports.ClientCredentials = void 0;
-var forms_1 = require("./forms");
-Object.defineProperty(exports, "FormMeta", { enumerable: true, get: function () { return forms_1.FormMeta; } });
-Object.defineProperty(exports, "FormField", { enumerable: true, get: function () { return forms_1.FormField; } });
-Object.defineProperty(exports, "FieldOption", { enumerable: true, get: function () { return forms_1.FieldOption; } });
-Object.defineProperty(exports, "InputType", { enumerable: true, get: function () { return forms_1.InputType; } });
-var ClientCredentials = /** @class */ (function () {
-    function ClientCredentials(username, password, serverUrl) {
-        this.username = username ? username : '';
-        this.password = password ? password : '';
-        this.serverUrl = serverUrl ? serverUrl : '';
+exports.FieldOption = exports.InputType = exports.ButtonType = exports.ValueType = exports.ButtonDto = exports.LayoutDto = exports.AvatarDto = exports.ClientDto = exports.ParentId = void 0;
+var ParentId = /** @class */ (function () {
+    function ParentId() {
     }
-    return ClientCredentials;
+    return ParentId;
 }());
-exports.ClientCredentials = ClientCredentials;
+exports.ParentId = ParentId;
+var ClientDto = /** @class */ (function () {
+    function ClientDto() {
+    }
+    return ClientDto;
+}());
+exports.ClientDto = ClientDto;
+var AvatarDto = /** @class */ (function (_super) {
+    __extends(AvatarDto, _super);
+    function AvatarDto() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.id = null;
+        return _this;
+    }
+    return AvatarDto;
+}(ParentId));
+exports.AvatarDto = AvatarDto;
+var LayoutDto = /** @class */ (function (_super) {
+    __extends(LayoutDto, _super);
+    function LayoutDto() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.id = null;
+        return _this;
+    }
+    return LayoutDto;
+}(ParentId));
+exports.LayoutDto = LayoutDto;
+var ButtonDto = /** @class */ (function (_super) {
+    __extends(ButtonDto, _super);
+    function ButtonDto() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.id = null;
+        return _this;
+    }
+    return ButtonDto;
+}(ParentId));
+exports.ButtonDto = ButtonDto;
+var ValueType;
+(function (ValueType) {
+    ValueType["Int"] = "int";
+    ValueType["Float"] = "float";
+    ValueType["Boolean"] = "boolean";
+})(ValueType = exports.ValueType || (exports.ValueType = {}));
+var ButtonType;
+(function (ButtonType) {
+    ButtonType["Button"] = "button";
+    ButtonType["Slider"] = "slider";
+    ButtonType["Toggle"] = "toggle";
+})(ButtonType = exports.ButtonType || (exports.ButtonType = {}));
+var InputType;
+(function (InputType) {
+    InputType["Text"] = "text";
+    InputType["Button"] = "button";
+    InputType["Checkbox"] = "checkbox";
+    InputType["Date"] = "date";
+    InputType["Email"] = "email";
+    InputType["File"] = "file";
+    InputType["Hidden"] = "hidden";
+    InputType["Number"] = "number";
+    InputType["Password"] = "password";
+    InputType["Radio"] = "radio";
+    InputType["Range"] = "range";
+    InputType["Reset"] = "reset";
+    InputType["Submit"] = "submit";
+    InputType["Time"] = "time";
+    InputType["Url"] = "url";
+    InputType["Label"] = "label";
+    InputType["Boolean"] = "boolean";
+    InputType["Select"] = "select";
+    InputType["Textarea"] = "textarea";
+})(InputType = exports.InputType || (exports.InputType = {}));
+var FieldOption = /** @class */ (function () {
+    function FieldOption() {
+    }
+    return FieldOption;
+}());
+exports.FieldOption = FieldOption;
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,84 @@
-import { FormMeta, FormField, FieldOption, InputType } from './forms';
-
-export class ClientCredentials {
-    serverUrl: string;
-    username: string;
-    password: string;
-    apiToken: string | undefined | null;
-
-    constructor(username?: string, password?: string, serverUrl?: string) {
-        this.username = username ? username : '';
-        this.password = password ? password : '';
-        this.serverUrl = serverUrl ? serverUrl : '';
-    }
-}
-
 export interface OscMessage {
     oscType: string;
     address: string;
     args: [boolean | number | string];
 }
 
-export { FormMeta, FormField, FieldOption, InputType };
+export class ParentId {
+    parentId?: string;
+}
+
+export class ClientDto {
+    id: string;
+    username: string;
+    displayName: string;
+    bio: string | null;
+    picture: string | null;
+    hidden: boolean;
+    avatars: AvatarDto[];
+}
+
+export class AvatarDto extends ParentId {
+    id: string = null;
+    vrcId: string;
+    label: string;
+    default: boolean;
+    layouts: LayoutDto[];
+}
+
+export class LayoutDto extends ParentId {
+    id: string = null;
+    label: string;
+    order: number;
+    buttons: ButtonDto[];
+}
+
+export class ButtonDto extends ParentId {
+    id: string = null;
+    label: string;
+    path: string;
+    value: string;
+    valueType: ValueType;
+    buttonType: ButtonType;
+    image: string | null;
+    order: number;
+}
+
+export enum ValueType {
+    Int = 'int',
+    Float = 'float',
+    Boolean = 'boolean',
+}
+
+export enum ButtonType {
+    Button = 'button',
+    Slider = 'slider',
+    Toggle = 'toggle',
+}
+
+export enum InputType {
+    Text = 'text',
+    Button = 'button',
+    Checkbox = 'checkbox',
+    Date = 'date',
+    Email = 'email',
+    File = 'file',
+    Hidden = 'hidden',
+    Number = 'number',
+    Password = 'password',
+    Radio = 'radio',
+    Range = 'range',
+    Reset = 'reset',
+    Submit = 'submit',
+    Time = 'time',
+    Url = 'url',
+    Label = 'label',
+    Boolean = 'boolean',
+    Select = 'select',
+    Textarea = 'textarea',
+}
+
+export class FieldOption {
+    key: string;
+    value: string;
+}
