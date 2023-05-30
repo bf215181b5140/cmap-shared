d2f857deadeb36c6040d7b23546efd749da7b82c
bf215181b5140 <108577982+bf215181b5140@users.noreply.github.com>
2023-05-30 17:03:04 +0200

    parameter button

--- dist/react/components/parameter.button.d.ts
+++ dist/react/components/parameter.button.d.ts
@@ -1,12 +1,16 @@
-import { ButtonDto, ButtonStyleDto, UsedButton } from '../../index';
+import { ButtonDto, ButtonStyleDto, ControlParameterDto, UsedButton } from '../../index';
 export declare const URL: string;
 interface ParameterButtonProps {
     button: ButtonDto;
     buttonStyle: ButtonStyleDto;
     active?: boolean;
     disabled?: boolean;
+    value?: string | number | boolean | undefined;
+    useCostParameter?: {
+        controlParameter: ControlParameterDto;
+        value: number;
+    };
     onClick?: (usedParameter: UsedButton) => void;
-    flexBasis?: string;
 }
-export default function ParameterButton(props: ParameterButtonProps): import("react/jsx-runtime").JSX.Element;
+export default function ParameterButtonTesting(props: ParameterButtonProps): import("react/jsx-runtime").JSX.Element;
 export {};
--- src/react/components/parameter.button.tsx
+++ src/react/components/parameter.button.tsx
@@ -1,6 +1,7 @@
 import styled, { css } from 'styled-components';
 import colors from '../../colors.json';
-import { ButtonDto, ButtonImageOrientation, ButtonStyleDto, ButtonType, UsedButton, ValueType } from '../../index';
+import { ButtonDto, ButtonImageOrientation, ButtonStyleDto, ButtonType, ControlParameterDto, UsedButton, ValueType } from '../../index';
+import expOrb from '../images/expOrb.png';
 
 export const URL = process.env.NODE_ENV === 'production' ? 'https://changemyavatarparams.com' : 'http://localhost:8080';
 
@@ -9,13 +10,14 @@ interface ParameterButtonProps {
     buttonStyle: ButtonStyleDto;
     active?: boolean;
     disabled?: boolean;
+    value?: string | number | boolean | undefined;
+    useCostParameter?: { controlParameter: ControlParameterDto, value: number };
     onClick?: (usedParameter: UsedButton) => void;
-    flexBasis?: string;
 }
- // todo paste from frontend
-export default function ParameterButton(props: ParameterButtonProps) {
 
-    function onClick(value?: number) {
+export default function ParameterButtonTesting(props: ParameterButtonProps) {
+
+    function onClick(value?: string) {
         if (props.onClick) {
             switch (props.button.buttonType) {
                 case ButtonType.Button:
@@ -25,24 +27,44 @@ export default function ParameterButton(props: ParameterButtonProps) {
                     props.onClick({buttonId: props.button.id, value: props.active ? props.button.valueAlt : props.button.value});
                     break;
                 case ButtonType.Slider:
-                    props.onClick({buttonId: props.button.id, value: 'value'});
+                    if (value === undefined) return;
+                    props.onClick({buttonId: props.button.id, value: value});
                     break;
             }
         }
     }
 
-    if (props.button.buttonType === ButtonType.Slider) {
-        return (<ParameterSliderStyled type="range" step={1} flexBasis={props.flexBasis} disabled={!!props.disabled} className={props.buttonStyle.className}
-                                       onClick={(e: any) => onClick(e.target.value)}
-                                       min={props.button.valueType === ValueType.Float ? (Number(props.button.value) * 100) : props.button.value}
-                                       max={props.button.valueType === ValueType.Float ? (Number(props.button.valueAlt) * 100) : props.button.valueAlt} />);
+    function useCostUsable(): boolean {
+        if (props.useCostParameter && props.button.useCost) {
+            return props.useCostParameter.value - props.button.useCost > Number(props.useCostParameter.controlParameter.valuePrimary);
+        }
+        return true;
     }
 
-    return (<ParameterButtonStyled flexBasis={props.flexBasis} disabled={!!props.disabled} className={props.buttonStyle.className} onClick={() => onClick()}>
-        {props.button.image && <ParameterButtonPicture src={URL + '/' + props.button.image} imageOrientation={props.button.imageOrientation} />}
-        {props.button.label && <ParameterButtonLabel>{props.button.label}</ParameterButtonLabel>}
-        <ActiveOverlay active={!!props.active} />
-    </ParameterButtonStyled>);
+    if (props.button.buttonType === ButtonType.Slider) {
+        return (<UseCostWrapper>
+            <SliderWrapper>
+                {props.button.label && <ParameterSliderLabel>{props.button.label}</ParameterSliderLabel>}
+                <ParameterSliderStyled type="range" step={1} disabled={!!props.disabled || !useCostUsable()}
+                                       className={props.buttonStyle.className}
+                                       onClick={(e: any) => onClick(e.target.value)}
+                                       defaultValue={typeof props.value === 'number' ? props.value : undefined}
+                                       min={props.button.valueType === ValueType.Float ? (Number(props.button.value) * 100) : props.button.value}
+                                       max={props.button.valueType === ValueType.Float ? (Number(props.button.valueAlt) * 100) : props.button.valueAlt} />
+            </SliderWrapper>
+            {props.button.useCost && <UseCostIcon position="top" background={expOrb} usable={useCostUsable()}>{props.button.useCost.toString()}</UseCostIcon>}
+        </UseCostWrapper>);
+    }
+
+    return (<UseCostWrapper>
+        <ParameterButtonStyled disabled={!!props.disabled || !useCostUsable()} className={`${props.buttonStyle.className} ${props.active ? 'active' : ''}`}
+                               onClick={() => onClick()}>
+            {props.button.image && <ParameterButtonPicture src={URL + '/' + props.button.image} imageOrientation={props.button.imageOrientation} />}
+            {props.button.label && <ParameterButtonLabel>{props.button.label}</ParameterButtonLabel>}
+            <ActiveOverlay active={!!props.active} />
+        </ParameterButtonStyled>
+        {props.button.useCost && <UseCostIcon position="bottom" background={expOrb} usable={useCostUsable()}>{props.button.useCost.toString()}</UseCostIcon>}
+    </UseCostWrapper>);
 }
 
 function imageOrientationToAspectRatio(imageOrientation: ButtonImageOrientation): string {
@@ -57,9 +79,7 @@ function imageOrientationToAspectRatio(imageOrientation: ButtonImageOrientation)
     }
 }
 
-const ParameterButtonStyled = styled.div<{ flexBasis?: string, disabled: boolean }>`
-  flex-basis: ${props => props.flexBasis ? props.flexBasis : '100%'};
-  align-self: flex-start;
+const ParameterButtonStyled = styled.div<{ disabled: boolean }>`
   text-align: center;
   padding: 0;
   min-height: 4.5em;
@@ -74,7 +94,7 @@ const ParameterButtonStyled = styled.div<{ flexBasis?: string, disabled: boolean
     border-radius: 8px;
     transition: 0.2s linear;
 
-    :hover {
+    :hover, &.active {
       transform: scale(1.02) perspective(1px);
       background: ${colors['button-hover-bg']};
       border: 2px solid ${colors['button-hover-border']};
@@ -87,7 +107,7 @@ const ParameterButtonStyled = styled.div<{ flexBasis?: string, disabled: boolean
     border-radius: 8px;
     transition: 0.2s linear;
 
-    :hover {
+    :hover, &.active {
       transform: scale(1.02) perspective(1px);
       background: ${colors['button-2-hover-bg']};
       border: 2px solid ${colors['button-2-hover-border']};
@@ -109,7 +129,7 @@ const ActiveOverlay = styled.div<{ active: boolean }>`
   bottom: 0;
   left: 0;
   right: 0;
-  
+
   ${props => {
     if (props.active) {
       return css`
@@ -124,7 +144,7 @@ const ActiveOverlay = styled.div<{ active: boolean }>`
             background-position: 0 100%
           }
         }
-        
+
         :after {
           content: '';
           position: absolute;
@@ -167,23 +187,33 @@ const ParameterButtonPicture = styled.div<{ src: string, imageOrientation: Butto
   background-size: cover;
 `;
 
-const ParameterSliderStyled = styled.input<{ flexBasis?: string, disabled: boolean }>`
-  flex-basis: ${props => props.flexBasis ? props.flexBasis : '100%'};
-  align-self: flex-start;
+const SliderWrapper = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const ParameterSliderLabel = styled.div`
+  padding: 5px;
+  margin: 0;
+  align-items: center;
+`;
+
+const ParameterSliderStyled = styled.input<{ disabled: boolean }>`
   padding: 4px;
   height: 36px;
   min-width: 180px;
   width: 100%;
-  //cursor: pointer;
-  //overflow: hidden;
   border-radius: 8px;
-
+  cursor: pointer;
   -webkit-appearance: none;
   background: ${colors['input-bg']};
 
+
+  // todo style thumb
   ::-webkit-slider-thumb {
-    height: 26px;
-    width: 20px;
+    height: 30px;
+    width: 30px;
     border-radius: 4px;
   }
 
@@ -216,4 +246,20 @@ const ParameterSliderStyled = styled.input<{ flexBasis?: string, disabled: boole
   }
 
   ${props => props.disabled ? disabledParamStyle : null};
+`;
+
+const UseCostWrapper = styled.div`
+  position: relative;
+`;
+
+const UseCostIcon = styled.div<{ background: string, usable: boolean, position: 'top' | 'bottom' }>`
+  position: absolute;
+  ${props => props.position === 'top' ? css`top: 10px;` : css`bottom: 5px;`};
+  right: 10px;
+  padding-right: 18px;
+  background: url(${props => props.background}) center right no-repeat;
+  background-size: 14px;
+  color: ${props => props.usable ? '#2ec92e' : '#dd3322'};
+  text-shadow: 0 0 3px black;
+  pointer-events: none;
 `;
