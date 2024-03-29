83d56778a0f01744866b772540cb8ecf62fe86e6
bf215181b5140 <108577982+bf215181b5140@users.noreply.github.com>
2024-03-29 06:44:14 +0100

    export directly from

--- dist/index.js
+++ dist/index.js
@@ -13,29 +13,36 @@ var register_1 = require("./types/register");
 Object.defineProperty(exports, "RegisterFormSchema", { enumerable: true, get: function () { return register_1.RegisterFormSchema; } });
 Object.defineProperty(exports, "RegisterSchema", { enumerable: true, get: function () { return register_1.RegisterSchema; } });
 Object.defineProperty(exports, "RegisterKeySchema", { enumerable: true, get: function () { return register_1.RegisterKeySchema; } });
-var shared_1 = require("./types/shared");
-Object.defineProperty(exports, "BaseIdSchema", { enumerable: true, get: function () { return shared_1.BaseIdSchema; } });
-Object.defineProperty(exports, "BaseParentIdSchema", { enumerable: true, get: function () { return shared_1.BaseParentIdSchema; } });
-Object.defineProperty(exports, "CmapApiError", { enumerable: true, get: function () { return shared_1.CmapApiError; } });
-Object.defineProperty(exports, "CmapApiErrorDTO", { enumerable: true, get: function () { return shared_1.CmapApiErrorDTO; } });
-Object.defineProperty(exports, "RequiredIdSchema", { enumerable: true, get: function () { return shared_1.RequiredIdSchema; } });
-Object.defineProperty(exports, "ParameterValueType", { enumerable: true, get: function () { return shared_1.ParameterValueType; } });
+// Profile
 var profile_1 = require("./types/profile");
 Object.defineProperty(exports, "ProfileFormSchema", { enumerable: true, get: function () { return profile_1.ProfileFormSchema; } });
+// Tiers
 var tier_1 = require("./types/tier");
 Object.defineProperty(exports, "TierType", { enumerable: true, get: function () { return tier_1.TierType; } });
+// Avatar
 var avatar_1 = require("./types/avatar");
 Object.defineProperty(exports, "AvatarFormSchema", { enumerable: true, get: function () { return avatar_1.AvatarFormSchema; } });
-var button_1 = require("./types/button");
-Object.defineProperty(exports, "ButtonFormSchema", { enumerable: true, get: function () { return button_1.ButtonFormSchema; } });
-Object.defineProperty(exports, "ButtonImageOrientation", { enumerable: true, get: function () { return button_1.ButtonImageOrientation; } });
-Object.defineProperty(exports, "ButtonType", { enumerable: true, get: function () { return button_1.ButtonType; } });
+// Control parameters
 var controlParameters_1 = require("./types/controlParameters");
 Object.defineProperty(exports, "ControlParametersFormSchema", { enumerable: true, get: function () { return controlParameters_1.ControlParametersFormSchema; } });
 Object.defineProperty(exports, "ControlParameterRole", { enumerable: true, get: function () { return controlParameters_1.ControlParameterRole; } });
+// Layout
 var layout_1 = require("./types/layout");
 Object.defineProperty(exports, "LayoutFormSchema", { enumerable: true, get: function () { return layout_1.LayoutFormSchema; } });
 Object.defineProperty(exports, "LayoutWidth", { enumerable: true, get: function () { return layout_1.LayoutWidth; } });
+// Button
+var button_1 = require("./types/button");
+Object.defineProperty(exports, "ButtonFormSchema", { enumerable: true, get: function () { return button_1.ButtonFormSchema; } });
+Object.defineProperty(exports, "ButtonType", { enumerable: true, get: function () { return button_1.ButtonType; } });
+Object.defineProperty(exports, "ButtonImageOrientation", { enumerable: true, get: function () { return button_1.ButtonImageOrientation; } });
+// Shared
+var shared_1 = require("./types/shared");
+Object.defineProperty(exports, "CmapApiError", { enumerable: true, get: function () { return shared_1.CmapApiError; } });
+Object.defineProperty(exports, "CmapApiErrorDTO", { enumerable: true, get: function () { return shared_1.CmapApiErrorDTO; } });
+Object.defineProperty(exports, "BaseIdSchema", { enumerable: true, get: function () { return shared_1.BaseIdSchema; } });
+Object.defineProperty(exports, "RequiredIdSchema", { enumerable: true, get: function () { return shared_1.RequiredIdSchema; } });
+Object.defineProperty(exports, "BaseParentIdSchema", { enumerable: true, get: function () { return shared_1.BaseParentIdSchema; } });
+Object.defineProperty(exports, "ParameterValueType", { enumerable: true, get: function () { return shared_1.ParameterValueType; } });
 var FieldOption = /** @class */ (function () {
     function FieldOption() {
         this.key = '';
--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,51 @@
 import { ReactNode } from 'react';
 import { IgnoredParams } from './const';
 import { theme } from './react/theme';
-// Login
-export { LoginSchema, LoginDTO, LoginTokenDTO } from './types/login';
-// Register
-export { RegisterRequestDTO, RegisterInfoDTO, RegisterFormSchema, RegisterSchema, RegisterKeySchema, RegisterDTO, RegisterFormDTO } from './types/register';
-import { RegisterRequestDTO, RegisterInfoDTO, RegisterFormSchema, RegisterSchema, RegisterKeySchema, RegisterDTO, RegisterFormDTO } from './types/register';
-import { BaseIdSchema, BaseParentIdSchema, CmapApiError, CmapApiErrorDTO, RequiredIdSchema, ParameterValueType } from './types/shared';
-import { BackgroundDTO } from './types/background';
-import { ButtonStyleDTO } from './types/buttonStyle';
-import { ProfileDTO, ProfileFormDTO, ProfileFormSchema, ClientDTO } from './types/profile';
-import { TiersDTO, TierDTO, TierType } from './types/tier';
-import { AvatarFormDTO, AvatarFormSchema, AvatarPageDTO, AvatarDTO } from './types/avatar';
-import { ButtonFormDTO, ButtonFormSchema, ButtonDTO, ButtonImageOrientation, ButtonType } from './types/button';
-import { ControlParametersFormDTO, ControlParametersFormSchema, ControlParameterDTO, ControlParameterRole } from './types/controlParameters';
-import { LayoutFormDTO, LayoutFormSchema, LayoutDTO, LayoutWidth } from './types/layout';
-import { FileUploadDTO, UploadedFileDTO } from './types/files';
-import { WebsitePageDTO } from './types/websitePage';
-import { ApproveFilesDTO } from './types/approveFiles';
+// import { RegisterRequestDTO, RegisterInfoDTO, RegisterFormSchema, RegisterSchema, RegisterKeySchema, RegisterDTO, RegisterFormDTO };
+// import { BaseIdSchema, BaseParentIdSchema, CmapApiError, CmapApiErrorDTO, RequiredIdSchema, ParameterValueType };
+// import { BackgroundDTO };
+// import { ButtonStyleDTO };
+// import { ProfileDTO, ProfileFormDTO, ProfileFormSchema, ClientDTO } from;
+// import { TiersDTO, TierDTO, TierType };
+// import { AvatarFormDTO, AvatarFormSchema, AvatarPageDTO, AvatarDTO };
+// import { ButtonFormDTO, ButtonFormSchema, ButtonDTO, ButtonImageOrientation, ButtonType };
+// import { ControlParametersFormDTO, ControlParametersFormSchema, ControlParameterDTO, ControlParameterRole };
+// import { LayoutFormDTO, LayoutFormSchema, LayoutDTO, LayoutWidth };
+// import { FileUploadDTO, UploadedFileDTO };
+// import { WebsitePageDTO };
+// import { ApproveFilesDTO };
 
 /* ------------------------------------------------------------ */
 // Types
 /* ------------------------------------------------------------ */
 // WebsitePage
-export { WebsitePageDTO };
+export { WebsitePageDTO } from './types/websitePage';
 // Admin approve files
-export { ApproveFilesDTO };
+export { ApproveFilesDTO } from './types/approveFiles';
+// Login
+export { LoginSchema, LoginDTO, LoginTokenDTO } from './types/login';
+// Register
+export { RegisterRequestDTO, RegisterInfoDTO, RegisterFormSchema, RegisterSchema, RegisterKeySchema, RegisterDTO, RegisterFormDTO } from './types/register';
 // Profile
-export { ProfileFormSchema, ProfileFormDTO, ProfileDTO, ClientDTO };
+export { ProfileFormSchema, ProfileFormDTO, ProfileDTO, ClientDTO } from './types/profile';
 // Background
-export { BackgroundDTO };
+export { BackgroundDTO } from './types/background';
 // ButtonStyle
-export { ButtonStyleDTO };
+export { ButtonStyleDTO } from './types/buttonStyle';
 // Tiers
-export { TiersDTO, TierDTO, TierType };
+export { TiersDTO, TierDTO, TierType } from './types/tier';
 // Avatar
-export { AvatarFormSchema, AvatarFormDTO, AvatarPageDTO, AvatarDTO };
+export { AvatarFormSchema, AvatarFormDTO, AvatarPageDTO, AvatarDTO } from './types/avatar';
 // Control parameters
-export { ControlParametersFormSchema, ControlParametersFormDTO, ControlParameterDTO, ControlParameterRole };
+export { ControlParametersFormSchema, ControlParametersFormDTO, ControlParameterDTO, ControlParameterRole } from './types/controlParameters';
 // Layout
-export { LayoutDTO, LayoutFormSchema, LayoutFormDTO, LayoutWidth };
+export { LayoutDTO, LayoutFormSchema, LayoutFormDTO, LayoutWidth } from './types/layout';
 // Button
-export { ButtonFormSchema, ButtonFormDTO, ButtonDTO, ButtonType, ButtonImageOrientation };
+export { ButtonFormSchema, ButtonFormDTO, ButtonDTO, ButtonType, ButtonImageOrientation } from './types/button';
 // Files
-export { UploadedFileDTO, FileUploadDTO };
+export { UploadedFileDTO, FileUploadDTO } from './types/files';
 // Shared
-export { CmapApiError, CmapApiErrorDTO, BaseIdSchema, RequiredIdSchema, BaseParentIdSchema, ParameterValueType };
+export { CmapApiError, CmapApiErrorDTO, BaseIdSchema, RequiredIdSchema, BaseParentIdSchema, ParameterValueType } from './types/shared';
 
 /* ------------------------------------------------------------ */
 // Other
