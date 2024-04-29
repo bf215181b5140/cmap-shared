83c3c5420795a6be80d5c821e858445b4ed5f0d6
bf215181b5140 <108577982+bf215181b5140@users.noreply.github.com>
2024-04-29 08:38:45 +0200

    Revert "UpdateClientInfoDTO"
    This reverts commit c6aade5f3cf6811876b31ff4caa6621a76de8db5.

--- src/types/update/index.ts
+++ src/types/update/index.ts
@@ -13,13 +13,7 @@ export const UpdatesFormSchema = z.object({
     versions: z.array(UpdateSchema),
 });
 
-export const UpdateClientInfoSchema = z.object({
-    version: z.string().min(5).max(16),
-    username: z.string().max(16).optional(),
-});
-
 export type UpdateDTO = z.infer<typeof UpdateSchema>;
 
 export type UpdatesFormDTO = z.infer<typeof UpdatesFormSchema>;
 
-export type UpdateClientInfoDTO = z.infer<typeof UpdateClientInfoSchema>;
