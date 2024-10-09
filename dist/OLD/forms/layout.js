869fb2e9515cd8b677b727fd9cbf03daf00c5ac0
bf215181b5140 <108577982+bf215181b5140@users.noreply.github.com>
2024-10-09 18:41:04 +0200

    API types

--- dist/OLD/forms/layout.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LayoutFormSchema = void 0;
-const shared_1 = require("../../shared");
-const zod_1 = require("zod");
-exports.LayoutFormSchema = shared_1.BaseNullableIdSchema.extend({
-    label: zod_1.z.string().min(3).max(32),
-    avatars: zod_1.z.array(shared_1.vrcAvatarIdSchema),
-});
--- src/OLD/forms/layout.ts
+++
@@ -1,9 +0,0 @@
-import { BaseNullableIdSchema, vrcAvatarIdSchema } from '../../shared';
-import { z } from 'zod';
-
-export const LayoutFormSchema = BaseNullableIdSchema.extend({
-    label: z.string().min(3).max(32),
-    avatars: z.array(vrcAvatarIdSchema),
-});
-
-export type LayoutFormDTO = z.infer<typeof LayoutFormSchema>;
