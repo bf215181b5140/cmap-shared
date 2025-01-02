8a08558a71831cd2301323f269312bf7252489bb
bf215181b5140 <108577982+bf215181b5140@users.noreply.github.com>
2025-01-02 21:00:03 +0100

    loading spinner

---
+++ react/components/loadingSpinner/loadingSpinner.component.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const LoadingSpinner = styled.div`
+  width: 100%;
+  height: 100%;
+  text-align: center;
+
+  :after {
+    content: '';
+    display: inline-block;
+    width: 48px;
+    height: 48px;
+    margin: 24px;
+    border: 5px solid #FFF;
+    border-bottom-color: transparent;
+    border-radius: 50%;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+
+    @keyframes rotation {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  }
+`;
