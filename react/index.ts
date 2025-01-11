e23bd744cc820e718f7928827d4812a8cb660765
bf215181b5140 <108577982+bf215181b5140@users.noreply.github.com>
2025-01-11 15:28:21 +0100

    LayoutButtonWrapper

--- react/index.ts
+++ react/index.ts
@@ -5,6 +5,7 @@ export { LayoutButton } from './components/button/layoutButton.component';
 export { ParameterButton } from './components/button/parameterButton.component';
 export { ParameterSlider } from './components/button/parameterSlider.component';
 export { PresetButton } from './components/button/presetButton.component';
+export { LayoutButtonWrapper } from './components/button/layoutButtonWrapper.component';
 export { LayoutGroup } from './components/group/layoutGroup.component';
 export { Layout, LayoutGroupGap } from './components/layout/layout.component';
 export { LoadingSpinner } from './components/loadingSpinner/loadingSpinner.component';
